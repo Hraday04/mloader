dbd9c5482405ebca175c13610589e340923cc422
Hraday04 <146326419+Hraday04@users.noreply.github.com>
2025-08-30 18:20:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Socket.io
+io.on("connection", (socket) => {
+  socket.on("client-message", (message) => {
+    io.emit("message", message);
+  });
+    
+    socket.on("file-upload", (fileData) => {
+        io.emit("file-upload", fileData);
+    });
+
+    socket.on("upvote", (fileUrl) => {
+        io.emit("upvote", fileUrl);
+    });
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
+
+server.listen(9000, () => console.log(`Server Started at PORT:9000`));
